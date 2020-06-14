451c0ca1fe2dbb8904b8093cbc169157f1532dc4
Amardeep T <amardeept@annait.net>
2020-06-14 23:05:22 +0530

    deployment

---
+++ client/build/precache-manifest.ecdadefbe1c1da1f3761b9f03ac0e8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a7b93e4af9339a1959c14e91641149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24c4185f6e4792822c7b",
+    "url": "/static/css/main.2bc944d8.chunk.css"
+  },
+  {
+    "revision": "f79ccf38a21490c53faa",
+    "url": "/static/js/2.4e36c187.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4e36c187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c4185f6e4792822c7b",
+    "url": "/static/js/main.ff7a5202.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,16 +10,19 @@ connectDB();
 //initiate middlware to accept json data
 app.use(express.json({ extended : false }));
 
-app.get('/', (req, res) => {
-    console.log("hello world");
-    //res.send('Hello world');
-    res.json({msg: 'Hello world'});
-});
 
 //Defining Routes through Middleware
 app.use('/api/users', require('./routes/users') );
 app.use('/api/contacts', require('./routes/contacts') );
 app.use('/api/auth', require('./routes/auth') );
+
+//Serve static assets in production
+if(process.env.NODE_ENV==='production'){
+    //SET THE STATIC FOLDER
+    app.use(express.static('client/build') );
+
+    app.get('*', (req, res) => res.sendFile( path.resolve( __dirname,'client', 'build', 'index.html' ) ) );
+}
 
 
 
